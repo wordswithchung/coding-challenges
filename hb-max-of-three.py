bc63282c10da85bc366396e94302f480e0a598e5
Chung Nguyen <chung.nguyen@gmail.com>
2016-11-15 22:37:27 -0800

    Completed max of three function

---
+++ hb-max-of-three.py
@@ -0,0 +1,18 @@
+"""
+Define a function max_of_three that takes in three numbers as arguments and
+returns the largest of them.
+
+>>> max_of_three(1, 5, 2)
+5
+
+>>> max_of_three(10, 1, 11)
+11
+"""
+
+def max_of_three(num1, num2, num3):
+    if num1 > num2 and num1 > num3:
+        return num1
+    elif num2 > num1 and num2 > num3:
+        return num2
+    else:
+        return num3
